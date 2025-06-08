b0d0dc587f65936dc5504f3f0732c2a3113cc916
jhapranay13 <jhapranay@gmail.com>
2025-06-07 22:11:19 -0700

    Adding Problems

---
+++ leetcode/BackTrackingRecursion/_2375_Construct_Smallest_Number_From_DI_String.java
@@ -0,0 +1,106 @@
+package leetcode.BackTrackingRecursion;
+
+/**
+ *
+ * You are given a 0-indexed string pattern of length n consisting of the characters 'I' meaning increasing and 'D' meaning decreasing.
+ *
+ * A 0-indexed string num of length n + 1 is created using the following conditions:
+ *
+ * num consists of the digits '1' to '9', where each digit is used at most once.
+ * If pattern[i] == 'I', then num[i] < num[i + 1].
+ * If pattern[i] == 'D', then num[i] > num[i + 1].
+ * Return the lexicographically smallest possible string num that meets the conditions.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: pattern = "IIIDIDDD"
+ * Output: "123549876"
+ * Explanation:
+ * At indices 0, 1, 2, and 4 we must have that num[i] < num[i+1].
+ * At indices 3, 5, 6, and 7 we must have that num[i] > num[i+1].
+ * Some possible values of num are "245639871", "135749862", and "123849765".
+ * It can be proven that "123549876" is the smallest possible num that meets the conditions.
+ * Note that "123414321" is not possible because the digit '1' is used more than once.
+ * Example 2:
+ *
+ * Input: pattern = "DDD"
+ * Output: "4321"
+ * Explanation:
+ * Some possible values of num are "9876", "7321", and "8742".
+ * It can be proven that "4321" is the smallest possible num that meets the conditions.
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= pattern.length <= 8
+ * pattern consists of only the letters 'I' and 'D'.
+ *
+ */
+public class _2375_Construct_Smallest_Number_From_DI_String {
+    class Solution {
+        public String smallestNumber(String pattern) {
+
+            for (int i = 0; i < nums.length; i++) {
+                nums[i] = i + 1;
+            }
+            int indexOfNum = 0;
+            // if D is a series which digit to start with
+            if (pattern.charAt(0) == 'D') {
+
+                while (indexOfNum < pattern.length() && pattern.charAt(indexOfNum) == 'D') {
+                    indexOfNum++;
+                }
+            }
+
+            while (indexOfNum < nums.length) {
+                StringBuilder holder = new StringBuilder();
+                holder.append(nums[indexOfNum]);
+                nums[indexOfNum] *= -1;
+
+                if (recur(pattern, 0, holder)) {
+                    return holder.toString();
+                }
+                nums[indexOfNum++] *= -1;
+            }
+            return "";
+        }
+        private int[] nums = new int[9];
+
+        private boolean recur(String pattern, int index, StringBuilder holder) {
+
+            if (pattern.length() == index) {
+                return true;
+            }
+            boolean ans = false;
+            int indx = -1;
+            int num = Integer.parseInt("" + holder.charAt(holder.length() - 1));
+            int end = nums.length;
+            // setting start and end depending on current index and current character
+            if (pattern.charAt(index) == 'I') {
+                indx = num;
+            } else {
+                indx = 0;
+                end = num;
+            }
+
+            while (indx < end) {
+                if (nums[indx] < 0) {
+                    indx++;
+                    continue;
+                }
+                holder.append(nums[indx]);
+                nums[indx] *= -1;
+
+                if (recur(pattern, index + 1, holder)) {
+                    ans = true;
+                    break;
+                }
+                nums[indx++] *= -1;
+                holder.deleteCharAt(holder.length() - 1);
+            }
+            return ans;
+        }
+    }
+}
---
+++ leetcode/Tree/_2673_Make_Costs_of_Paths_Equal_in_a_Binary_Tree.java
@@ -0,0 +1,117 @@
+package leetcode.Tree;
+
+/**
+ *
+ * You are given an integer n representing the number of nodes in a perfect binary tree consisting of nodes numbered from 1 to n. The root of the tree is node 1 and each node i in the tree has two children where the left child is the node 2 * i and the right child is 2 * i + 1.
+ *
+ * Each node in the tree also has a cost represented by a given 0-indexed integer array cost of size n where cost[i] is the cost of node i + 1. You are allowed to increment the cost of any node by 1 any number of times.
+ *
+ * Return the minimum number of increments you need to make the cost of paths from the root to each leaf node equal.
+ *
+ * Note:
+ *
+ * A perfect binary tree is a tree where each node, except the leaf nodes, has exactly 2 children.
+ * The cost of a path is the sum of costs of nodes in the path.
+ *
+ *
+ * Example 1:
+ *                  Node : point
+ *                   1 : 1
+ *                 /        \
+ *              2 : 5       3 : 2
+ *             /    \       /    \
+ *          4 : 2  5 : 3   6 : 3   7 : 1
+ *
+ * Input: n = 7, cost = [1,5,2,2,3,3,1]
+ * Output: 6
+ * Explanation: We can do the following increments:
+ * - Increase the cost of node 4 one time.
+ * - Increase the cost of node 3 three times.
+ * - Increase the cost of node 7 two times.
+ * Each path from the root to a leaf will have a total cost of 9.
+ * The total increments we did is 1 + 3 + 2 = 6.
+ * It can be shown that this is the minimum answer we can achieve.
+ * Example 2:
+ *
+ *                 Node : point
+ *                    1 : 5
+ *                 /        \
+ *              2 : 5       3 : 3
+ *
+ * Input: n = 3, cost = [5,3,3]
+ * Output: 0
+ * Explanation: The two paths already have equal total costs, so no increments are needed.
+ *
+ *
+ * Constraints:
+ *
+ * 3 <= n <= 105
+ * n + 1 is a power of 2
+ * cost.length == n
+ * 1 <= cost[i] <= 104
+ *
+ *
+ */
+
+public class _2673_Make_Costs_of_Paths_Equal_in_a_Binary_Tree {
+    public int minIncrements(int n, int[] cost) {
+        maxi = recurMax(cost, 0);
+        recur(cost, 0, 0);
+        return ans;
+    }
+    private int ans = 0;
+    private int maxi = 0;
+
+    private int recurMax(int[] cost, int index) {
+
+        if (index >= cost.length) {
+            return 0;
+        }
+        int currCost = cost[index];
+        int left = recurMax(cost, index * 2 + 1);
+        int right = recurMax(cost, index * 2 + 2);
+        return currCost + Math.max(left, right);
+    }
+
+    private int recur(int[] cost, int index, int sum) {
+
+        if (index * 2 + 1 >= cost.length) {
+            return maxi - (sum + cost[index]); // returning the deficit
+        }
+        int currCost = cost[index];
+
+        int left = recur(cost, index * 2 + 1, sum + currCost);
+        int right = recur(cost, index * 2 + 2, sum + currCost);
+        // we can caluclate the difference. That difference is what
+        // the minimu side will have to increment to make the values of both path
+        // equal to maximium
+        int diff = Math.abs(left - right);
+        ans += diff;  // adjusting the difference
+
+        return Math.min(left, right);
+    }
+    //=============================================================================================
+    // Another approach using DFS
+    public int minIncrements1(int n, int[] cost) {
+        recur(cost, 0);
+        return ans;
+    }
+    //private int ans = 0;
+
+    private int recur(int[] cost, int index) {
+
+        if (index * 2 + 1 >= cost.length) {
+            return cost[index]; // returning the deficit
+        }
+        int currCost = cost[index];
+
+        int left = recur(cost, index * 2 + 1);
+        int right = recur(cost, index * 2 + 2);
+        // trying to make both paths equal
+        // finally all paths will be equal to the max path
+        int diff = Math.abs(left - right);
+        ans += diff;  // adjusting the difference on current level
+        // Since lower is now adjusted to max send max up
+        return currCost + Math.max(left, right);
+    }
+}
