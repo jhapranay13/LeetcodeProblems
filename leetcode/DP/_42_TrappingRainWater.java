089b2a0cb6222e9f13515fdff7fc34e571dab603
jhapranay13 <jhapranay@gmail.com>
2026-01-10 20:22:43 -0700

    New Solution Understanding

--- leetcode/DP/_42_TrappingRainWater.java
+++ leetcode/DP/_42_TrappingRainWater.java
@@ -166,4 +166,33 @@ public class _42_TrappingRainWater {
 		}
 		return ans;
 	}
+    //=============================================================================================
+    // Constant memory
+    public int trap2(int[] height) {
+
+        if (height == null || height.length == 0) return 0;
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return water;
+    }
 }
